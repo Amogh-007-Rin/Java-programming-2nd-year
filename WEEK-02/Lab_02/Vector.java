cfeb9bd536d542e6bd5a20e8ad123835c8d490a2
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-05 01:54:16 +0100

    Step-2

--- WEEK-02/Lab_02/Vector.java
+++ WEEK-02/Lab_02/Vector.java
@@ -1,34 +1,46 @@
 public class Vector {
-    private String name;
-    private int age;
-    private int studentId;
+    double x, y;
 
-    // Constructor
-    public Vector(String name, int age, int studentId) {
-        this.name = name;
-        this.age = age;
-        this.studentId = studentId;
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public String getName() {
-        return name;
+    public Vector scale(double factor) {
+        return new Vector(this.x * factor, this.y * factor);
     }
 
-    public int getAge() {
-        return age;
+    public Vector subtract(Vector other) {
+        return new Vector(this.x - other.x, this.y - other.y);
     }
 
-    public int getStudentId() {
-        return studentId;
+    public double length() {
+        return Math.sqrt(x * x + y * y);
     }
 
-    @Override
-    public String toString() {
-        return "Student{name='" + name + "', age=" + age + ", studentId=" + studentId + "}";
+    public Vector add(Vector other) {
+        return new Vector(this.x + other.x, this.y + other.y);
+    }
+
+    public void printVector() {
+        System.out.println("Vector x: " + x + ", y: " + y);
+        System.out.println("Vector length: " + length());
     }
 
     public static void main(String[] args) {
-        Vector v1 = new Vector("Amogh", 20, 24168333);
-        System.out.println(v1);
+        Vector v1 = new Vector(3, 4);
+        Vector v2 = new Vector(1, 2);
+
+        v1.printVector();
+        v2.printVector();
+
+        Vector v3 = v1.add(v2);
+        v3.printVector();
+
+        Vector v4 = v1.subtract(v2);
+        v4.printVector();
+
+        Vector v5 = v1.scale(2);
+        v5.printVector();
     }
 }
